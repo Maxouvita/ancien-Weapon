fb37eed84131428afa9398ecdb266b04ea55602e
Maxouvita <maxouvita@gmail.com>
2017-06-10 10:21:04 +0200

    dernier

--- Arme.cpp
+++ Arme.cpp
@@ -10,21 +10,17 @@ Arme::Arme(){
   _rld_time = 200;
   _cadence = 60;
   _orientation = 0;
-  _state_rld = false;
-  _state_tir = false;
-  _state_dflt = true;
+  _state = 0;
   _nb_balle = 30;
 }
 
 // Constructeur surchargé
-Arme::Arme(string name, int rld_time, int cadence, int orientation, bool state_rld, bool state_tir, bool state_dflt, int nb_balle){
+Arme::Arme(string name, int rld_time, int cadence, int orientation, int state, int nb_balle){
   _name = name;
   _rld_time = rld_time;
   _cadence = cadence;
   _orientation = orientation;
-  _state_rld = state_rld;
-  _state_tir = state_tir;
-  _state_dflt = state_dflt;
+  _state = state;
   _nb_balle = nb_balle;
 }
 
@@ -43,14 +39,8 @@ void Arme::set_cadence(int cadence){
 void Arme::set_orientation(int orientation){
   this->_orientation = orientation;
 }
-void Arme::set_state_rld(bool state_rld){
-  this->_state_rld = state_rld ;
-}
-void Arme::set_state_tir(bool state_tir){
-  this->_state_tir = state_tir ;
-}
-void Arme::set_state_dflt(bool state_dflt){
-  this->_state_dflt = state_dflt;
+void Arme::set_state(int state){
+  this->_state = state ;
 }
 void Arme::set_nb_balle(int nb_balle){
   this->_nb_balle = nb_balle;
--- Arme.h
+++ Arme.h
@@ -12,24 +12,27 @@ private:
   int _orientation;
   int _texture_list;
   int _dimension;
-  bool _state_rld;
-  bool _state_tir;
-  bool _state_dflt;
+  int _state;
   int _nb_balle;
 
 public:
   // Constructeurs
   Arme();
-  Arme(string _name, int _rld_time, int _cadence, int _orientation, bool _state_rld, bool _state_tir, bool _state_dflt, int _nb_balle);
-  // GETTERS à faire
+  Arme(string _name, int _rld_time, int _cadence, int _orientation, int _state, int _nb_balle);
+  // GETTERS
+  int get_name();
+  int get_rld_time();
+  int get_cadence();
+  int get_orientation();
+  int get_state();
+  int get_nb_balle();
+
   // SETTERS
   void set_name(string _name);
   void set_rld_time(int _rld_time);
   void set_cadence(int _cadence);
   void set_orientation(int _orientation);
-  void set_state_rld(bool _state_rld);
-  void set_state_tir(bool _state_tir);
-  void set_state_dflt(bool _state_dflt);
+  void set_state(int _state);
   void set_nb_balle(int _nb_balle);
 };
 #endif
